c61536e6a98a34e14a3403324de61f97566ba68e
kimyerak <yerak213@gmail.com>
2024-08-19 20:53:29 +0900

    feat: room api 5개

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-// import { RoomModule } from './room/room.module';
+import { RoomModule } from './room/room.module';
 // import { MoneyModule } from './money/money.module';
 // import { ChatModule } from './chat/chat.module';
 import { ConfigModule } from '@nestjs/config';
@@ -14,7 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRoot('mongodb://localhost:27017/poor-chat'), // 기본 URI 설정
     AuthModule,
     UserModule,
-    // RoomModule,
+    RoomModule,
     // MoneyModule,
     // ChatModule,
   ],
